3599f4044095359aa1de3d3a2f125dffb46a19eb
LauraHerrero1910 <laura.herrerogutierrez@usp.ceu.es>
2026-04-23 12:12:04 +0200

    Progreso Menu Pausa
    Integrado el boton de exit (quedan el de save y continue ) ademas de hacer que se pueda llamar desde cualquier ventana del gameplay

---
+++ cookincat/MenuPausa.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_save_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit() #Lee las escenas con tree y sale de todas
