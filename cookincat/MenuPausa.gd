f111e95d98586b0fe1a8d40e675db42b90f2e82e
LauraHerrero1910 <laura.herrerogutierrez@usp.ceu.es>
2026-04-24 10:50:53 +0200

    Añadido MenuPause
    -Añadido a las escenas (se activa con la tecla ESC)
    
    -Continue y Exit funcionan
    
    -Falta hacer un sistema de guardado para el boton Save

--- cookincat/MenuPausa.gd
+++ cookincat/MenuPausa.gd
@@ -3,21 +3,28 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$MenuControl.visible=false #Hace que este control pase de ser visible a no visible
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+#Cuando pulsa ESC aparece el Menu de Pausa
+func _input(event):
+	if event.is_action_pressed("Menu_Pausa"): #si al detectar input detecta el de MenuPausa (ESC) va a la funcion de abajo
+		$MenuControl.visible=true #El menu se hace visible
+		
+		
+#CONTINUE
 func _on_continue_pressed() -> void:
-	pass # Replace with function body.
+	$MenuControl.visible=false #Hace que este control pase de ser visible a no visible
+	
 
-
+#SAVE
 func _on_save_pressed() -> void:
 	pass # Replace with function body.
 
-
+#EXIT
 func _on_exit_pressed() -> void:
 	get_tree().quit() #Lee las escenas con tree y sale de todas
