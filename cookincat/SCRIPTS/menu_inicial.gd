be42d4fbcaaeafb38cb6450424b504c303bf09d9
doodles-art <laura.guillenpayo@usp.ceu.es>
2026-04-29 12:50:08 +0200

    ISSUE 6 COMPLETO
    Botones funcionales menú inicial, conectados con Cultivos y Opciones.

--- cookincat/SCRIPTS/menu_inicial.gd
+++ cookincat/SCRIPTS/menu_inicial.gd
@@ -1,11 +1,17 @@
 extends Control
 
+func on_PLAY_pressed():
+	get_tree().change_scene_to_file("res://ESCENAS/Pantalla_Cultivo.tscn")
+
+func on_OPTIONS_pressed():
+	get_tree().change_scene_to_file("res://ESCENAS/Menu_Opciones.tscn")
+
+func on_ExitGAME_pressed():
+	get_tree().quit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
