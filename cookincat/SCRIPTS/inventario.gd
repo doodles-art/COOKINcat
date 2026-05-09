436d208fd6815053bc6e4f353e1c0a32091b2ff2
LauraHerrero1910 <laura.herrerogutierrez@usp.ceu.es>
2026-05-09 23:37:44 +0200

    Progreso Inventario

--- cookincat/SCRIPTS/inventario.gd
+++ cookincat/SCRIPTS/inventario.gd
@@ -4,6 +4,7 @@ class_name Inventory
 
 signal inventario_actualizado
 
+
 var db:SQLite
 
 #lista de los bolsillos del inventario del jugador
@@ -21,12 +22,12 @@ func _cargar_inventario():
 	bolsillos.clear() #para no repetir filas las elimino y me aseguro de que no pase esto 
 	#SOLO QUIERO DE CULTIVOS E ITEMS LOS DATOS QUE ME INTERESEN PARA LOS OBJETOS ACTUALES DEL INVENTARIO
 	db.query("Select * FROM INVENTARIO AS INV LEFT JOIN ITEMS AS ITM ON INV.ID_Item=ITM.ID_Item LEFT JOIN CULTIVOS AS CLT ON ITM.ID_Cultivo=CLT.ID")
-	
+	print(db.query_result)
 	#PASO LA INFORMACION DE CADA FILA DE LA TABLA EN SQLLITE A EL ARRAY/DICCIONARIO EN GODOT (PARA ACCEDER A EL SIN NECESIDAD DE HACER QUERY toDO EL RATO)
 	for row in db.query_result:
 		var bolsillo:=Bolsillo.new() #creo un "objeto" de tipo Bolsillo
 		bolsillo.id_bolsillo=row["ID_Bolsillo"]
-		bolsillo.id_item=row["ID_Item"]
+		bolsillo.item.id=row["ID_Item"]
 		bolsillo.tipo=row["Tipo"]
 		bolsillo.icono=row["Icono"]
 		bolsillo.cantidad=row["Cantidad"]
@@ -35,32 +36,33 @@ func _cargar_inventario():
 		bolsillo.item=Database.Diccionario_Item[bolsillo.id_bolsillo]
 		bolsillos[bolsillo.id_bolsillo]=bolsillo #lo añado con esta info al inventario (De sql a godot)
 		emit_signal("inventario_actualizado")#envio unaq señal de que he actualizado el inventario
-	
+		
 		
 	
 func _sumarItem(id_objeto:int, cantidad:int):
-	#busco si ya hay un objeto del mismo tipo ya en el inventario
-	for bolsillo in bolsillos.values():
-		
-		if(bolsillo.id_bolsillo==id_objeto): #lo encuentra
-			bolsillo.cantidad+=cantidad
-			db.query("UPDATE INVENTARIO SET Cantidad=? WHERE ID_Bolsillo=?
-			[bolsillo.cantidad,bolsillo.id_bolsillo]")
-			return #sale de la funcion y acaba
+	
+	if bolsillos.is_empty()==true: #si no hay ningun elemento en el diccionario (inventqrio vacio)
+		_insertarBolsillo(id_objeto,cantidad) #creo un nuevo bolsillo con este
+	
+	if bolsillos.is_empty()==false: #hay al menos un elemento en el diccionario (ahora tengo que ver si el tipo de elemento que quiero insertar se encuentra ya en el inventario)
+
+		#busco si ya hay un objeto del mismo tipo ya en el inventario
+		for bolsillo in bolsillos.values():
 			
-		#no lo encuentra
-		var nuevo_bolsillo:=Bolsillo.new()
-		nuevo_bolsillo.id_bolsillo=bolsillos.size()+1 #aumento el tamaño de la lista
-		nuevo_bolsillo.id_item=id_objeto
-		nuevo_bolsillo.cantidad=cantidad
-		
-		bolsillos[nuevo_bolsillo.id_bolsillo]=nuevo_bolsillo #añado este bolsillo con esta info al inventario
-		db.query("INSERT INTO INVENTARIO (ID_Bolsillo,ID_Item,Cantidad) VALUES ([nuevo_bolsillo.id_bolsillo,id_objeto,cantidad])")
-		
+			if(bolsillo.item.id==id_objeto): #lo encuentra
+				bolsillo.cantidad+=cantidad
+				db.query("UPDATE INVENTARIO SET Cantidad=? WHERE ID_Bolsillo=?
+				[bolsillo.cantidad,bolsillo.id_bolsillo]")
+				return #sale de la funcion y acaba
+			
+			else:#no lo encuentra (entonces tenemos que crear otro bolsillo con este "nuevo" tipo de dato
+				_insertarBolsillo(id_objeto,cantidad)
+
 	print("añadio")
-	print(id_objeto,cantidad)
+	print(id_objeto,cantidad)#compuebo
 	emit_signal("inventario_actualizado")#envio unaq señal de que he actualizado el inventario
 	
+
 func _restarItem(id_objeto:int,cantidad:int)->void:
 	emit_signal("inventario_actualizado")#envio unaq señal de que he actualizado el inventario
 	#busco si hay un objeto del mismo tipo en el inventario
@@ -79,4 +81,14 @@ func _restarItem(id_objeto:int,cantidad:int)->void:
 				db.query("DELETE FROM INVENTARIO WHERE ID_Bolsillo=?,[id_bolsillo,bolsillos.erase(id_bolsillo)]")	
 				
 			return
+
+#funcion que uso para insertar un nuevo bolsillo al inventario (lo uso para cuando no haya ningun bolsillo en la tabla o para cuando no haya ningun item de ese tipo todavia en el inventario)
+func _insertarBolsillo(id_objeto:int,cantidad:int)->void:
+	var nuevo_bolsillo:=Bolsillo.new()
+	nuevo_bolsillo.id_bolsillo=bolsillos.size()+1 #aumento el tamaño de la lista
+	nuevo_bolsillo.item = Database.Diccionario_Item[id_objeto]
+	nuevo_bolsillo.cantidad=cantidad
 		
+	bolsillos[nuevo_bolsillo.id_bolsillo]=nuevo_bolsillo #añado este bolsillo con esta info al inventario
+	db.query("INSERT INTO INVENTARIO (ID_Bolsillo, ID_Item, Cantidad) VALUES (%d, %d, %d)" % [nuevo_bolsillo.id_bolsillo,id_objeto,cantidad])
+	print(db.query_result)#comprueba
