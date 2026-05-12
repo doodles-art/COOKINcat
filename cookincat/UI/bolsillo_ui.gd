fef24deb1295d0968f740644e2a60e95f53155b9
LauraHerrero1910 <laura.herrerogutierrez@usp.ceu.es>
2026-05-12 11:53:28 +0200

    Progreso Drag&Drop
    Ahora arrastra objetos del inventario (tengo que arreglar la x que aparece cuando se arrastra)

--- cookincat/UI/bolsillo_ui.gd
+++ cookincat/UI/bolsillo_ui.gd
@@ -19,3 +19,35 @@ func _set_bolsillo(bolsillo:Bolsillo): #le paso algo de tipo Bolsillo en el que 
 		itemSprite.visible=true
 		itemSprite.texture=load(bolsillo.item.icon_texture_path)
 		itemCantidad.text=str(bolsillo.cantidad)
+
+
+#funcion llamada cuando haces click y empiezas a desplazar (devuelves los datos de lo que quieres mover)
+func _get_drag_data(at_position: Vector2) -> Variant:
+	
+	#si no esta sobre ninguna textura(celda)
+	if itemSprite== null:
+		return
+	 
+	#cuando arrastras se muestra una preview del objeto al lado del raton
+	var preview=itemSprite.duplicate() #duplica la textura de la celda
+
+	#para centrar la preview
+	var c=Control.new()
+	c.add_child(preview)
+	#cetnro la preview en el raton que si no da toc
+	preview.position-=Vector2(40,25)
+	preview.size-=Vector2(20,20)
+	
+	
+	set_drag_preview(c) ##lo pone debajo del mouse mientras lo arrastras
+	
+	
+	return itemSprite
+	
+#Se llama si se verifica que se este arrasrrastrando algo (y si se puede soltar o no)
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return true
+
+#Si pasa el can_drop data ocurre este (decidimos que hacer con lo que se esta arrastrando)
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	pass
